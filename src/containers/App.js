289077096a530312dcddbe49b9b4b6382170f157
Peter <peterkingsonchan@gmail.com>
2020-08-14 12:47:39 +0100

    YTS-search-component
    Create default axios file. Set up search request to YouTubes' API

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import Search from '../components/Search';
+import youtube from '../API/youtube';
+
+const KEY = 'AIzaSyAU7KBz7GXto3hi5KckYtx7Ycgsy4fFXaY';
 
 class App extends Component {
+    state = {
+        videos: []
+    }
+
+    onSearchSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                part: 'snippet',
+                maxResuls: 10,
+                q: term,
+                safeSearch: 'none',
+                type: 'video',
+                key: KEY
+            }
+        })
+        console.log(response.data.items)
+        this.setState({ videos: response.data.items })
+    }
+
     render() {
         return (
-            <div>Hello</div>
+            <div className="ui container">
+                <h1 className="ui header">Search YouTube Videos</h1>
+                <Search onSearchSubmit={ this.onSearchSubmit } />
+            </div>
         )
     }
 }
